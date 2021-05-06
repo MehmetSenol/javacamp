7e4e91782904e6a2a415b8b8db51b3433416e69e
MehmetSenol <83767474+MehmetSenol@users.noreply.github.com>
2021-05-06 21:40:40 +0300

    Mehmet Senol
    odev1

---
+++ Homework/src/homework/Lessons.java
@@ -0,0 +1,21 @@
+
+package homework;
+
+
+public class Lessons {
+    int id;
+    String name;
+    String detail;
+    String teacher;
+
+    public Lessons(int id, String name, String detail,String teacher) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+        this.teacher=teacher;
+    }
+
+    public Lessons() {
+        
+    }
+}
