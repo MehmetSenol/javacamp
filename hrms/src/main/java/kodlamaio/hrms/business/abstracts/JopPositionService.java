96d413bbe208499bbf98a00e0178236646309786
MehmetSenol <83767474+MehmetSenol@users.noreply.github.com>
2021-05-12 14:55:55 +0300

    Delete hrms directory

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/JopPositionService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import kodlamaio.hrms.entities.concretes.JobPosition;
-
-import java.util.List;
-
-public interface JopPositionService {
-    List<JobPosition> getAll();
-}
